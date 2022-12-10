f79e59544804d06ab2e79a7ded4e8288c6dd82c1
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-10 21:19:02 +0800

    修改：内联CSS

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 function rand(){ 
  min=$1 
  max=$(($2-$min+1)) 
- num=$(($RANDOM+1000000000)) #增加一个10位的数再求余 
+ num=$(($RANDOM+1000000000)) # 增加一个10位的数再求余 
  echo $(($num%$max+$min)) 
 }  
-rnd=$(rand 100000 999999) 
+rnd=$(rand 100000 999999) # 六位验证码
 echo $rnd
 sed -i "s/otp-code/$rnd/g" otp.html 
 exit 0
