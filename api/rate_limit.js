1255270153e57bb554b1d5cded5c8eac1427878a
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-04-22 15:12:16 +0800

    MMMMDDDDDDDDDDDDDDDDDdddCCCCCCCCCCCCCCCCCCCCCCCCCCcSB

--- api/rate_limit.js
+++ api/rate_limit.js
@@ -1,20 +1,23 @@
 module.exports = (request, response) => {
     try {
-        const body = request.body;
+        const token = request.body,token;
     } catch (e) {
         return response.status(400).json({ error: e });
     }
-/*
-    fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-        body: `secret=0x4AAAAAAABBzssgfcpBXVvAoaR03SGp6Gg&response=${request.body.token}`,
-        method: 'POST',
-    });
-    // if (!result.json().success) return response.status(400).send("Failed");
-    // The Turnstile token was successfuly validated. Proceed with your application logic.
-    // Validate login, redirect user, etc.
-    // For this demo, we just echo the "/siteverify" response:
-    */
+    /*
+        fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+            body: `secret=0x4AAAAAAABBzssgfcpBXVvAoaR03SGp6Gg&response=${request.body.token}`,
+            method: 'POST',
+        });
+        // if (!result.json().success) return response.status(400).send("Failed");
+        // The Turnstile token was successfuly validated. Proceed with your application logic.
+        // Validate login, redirect user, etc.
+        // For this demo, we just echo the "/siteverify" response:
+        */
 
     console.log(request.body)
-    return response.status(200).send("Vaildate OK")
+    return response.status(200).json({
+        code: 0,
+        token: request.body.token
+    })
 }
