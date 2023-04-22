b6dd326bdcce6724b078021c966856ab1c20fd12
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-04-22 14:54:46 +0800

    dmd

--- api/rate_limit.js
+++ api/rate_limit.js
@@ -2,25 +2,25 @@ export default function handler(request, response) {
     const SECRET_KEY = '0x4AAAAAAABBzssgfcpBXVvAoaR03SGp6Gg';
     try {
         const body = request.body;
-      } catch (error) {
-        return response.status(400).json({ error: 'request.body' });
+      } catch (e) {
+        return response.status(400).json({ error: e });
       }
     // Turnstile injects a token in "cf-turnstile-response".
-    const token = body.cf-turnstile-response || null;
+    const token = body.token || null;
     // Validate the token by calling the "/siteverify" API.
-    let formData = `secret=${SECRET_KEY}&response=${token}`
+    let Data = `secret=${SECRET_KEY}&response=${token}`
 
     const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-        body: formData,
+        body: Data,
         method: 'POST',
     });
 
     const outcome = await result.json();
     if (!outcome.success) {
-        response.status(400).send("Failed")
+        return response.status(400).send("Failed")
     }
     // The Turnstile token was successfuly validated. Proceed with your application logic.
     // Validate login, redirect user, etc.
     // For this demo, we just echo the "/siteverify" response:
-    response.status(200).send("Vaildate OK")
+    return response.status(200).send("Vaildate OK")
 }
